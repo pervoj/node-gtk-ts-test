6b501c1d3334e8c2873400f29b6affe481c37572
Vojtěch Perník <develop@pervoj.cz>
2024-04-12 20:30:19 +0200

    Make the app translatable

--- src/win.ts
+++ src/win.ts
@@ -1,6 +1,7 @@
 import Adw from "@girs/node-adw-1";
 import Gtk from "@girs/node-gtk-4.0";
 import gi from "@girs/node-gtk";
+import { gettext as _ } from "libintl";
 import Template from "./templates";
 
 export default class MainWindow extends Adw.ApplicationWindow {
@@ -16,7 +17,7 @@ export default class MainWindow extends Adw.ApplicationWindow {
     super({ application: app });
 
     this._button.on("clicked", () => {
-      console.log("The button was pressed!");
+      console.log(_("The button was pressed!"));
     });
   }
 }

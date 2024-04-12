993a1d6e0ef212b873ebc9bcf9cadb31bb6d3372
Vojtěch Perník <develop@pervoj.cz>
2024-04-12 20:30:03 +0200

    Add i18n base

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import Adw from "@girs/node-adw-1";
 import Gio from "@girs/node-gio-2.0";
+import {
+  setLocale,
+  bindTextDomain,
+  bindTextDomainCodeset,
+  textDomain,
+  LC,
+} from "libintl";
 import MainWindow from "./win";
 import loadResources from "./resources";
+import { GETTEXT_PACKAGE, LOCALE_DIR } from "./constants";
 
 export default function main(args: string[]) {
+  setLocale(LC.all, "");
+  bindTextDomain(GETTEXT_PACKAGE, LOCALE_DIR);
+  bindTextDomainCodeset(GETTEXT_PACKAGE, "UTF-8");
+  textDomain(GETTEXT_PACKAGE);
+
   loadResources();
 
   const app = new Adw.Application({
